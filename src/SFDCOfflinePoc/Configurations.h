e81a5907c073357265eb9dd97254d8be315e8524
Kyle Bowerman <kyle.bowerman@gmail.com>
2016-02-11 11:57:13 -0600

    FS2 signature submission 22089

---
+++ src/SFDCOfflinePoc/Configurations.h
@@ -0,0 +1,70 @@
+//
+//  Configurations.h
+//
+//  Created by TCCODER on 1/31/16.
+//  Copyright (c) 2016 Topcoder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+/**
+ @class Configurations
+ @discussion This class loads configurable constants from a config.plist
+ 
+ @author TCCODER
+ @version 1.0
+ */
+
+@interface Configurations : NSObject
+
+/**
+ *  pdf name
+ *
+ *  @return pdf name
+ */
++ (NSString*)pdfName;
+
+/**
+ *  pdf height
+ *
+ *  @return pdf height
+ */
++ (CGFloat)pdfHeight;
+
+/**
+ *  pdf width
+ *
+ *  @return pdf width
+ */
++ (CGFloat)pdfWidth;
+
+/**
+ *  pdf content padding 
+ *
+ *  @return pdf padding
+ */
++ (CGFloat)pdfPadding;
+
+/**
+ *  pdf row height
+ *
+ *  @return pdf row height
+ */
++ (CGFloat)pdfRowHeight;
+
+/**
+ *  pdf row header height
+ *
+ *  @return pdf row header height
+ */
++ (CGFloat)pdfHeaderHeight;
+
+/**
+ *  pdf font size for text
+ *
+ *  @return pdf font size
+ */
++ (CGFloat)pdfFontSize;
+
+@end
